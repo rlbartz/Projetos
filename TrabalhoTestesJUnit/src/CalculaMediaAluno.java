6c8e97385a8ff2870c1f892cef3eab234493ecea
rlbartz <infobartz@gmail.com>
2012-09-22 16:07:41 -0300

    Inclusão classe calculo media

---
+++ TrabalhoTestesJUnit/src/CalculaMediaAluno.java
@@ -0,0 +1,21 @@
+public class CalculaMediaAluno {
+
+	public float caclularMedia(int notaPrimeiroBimestre,
+			int notaSegundoBimestre, int notaTerceiroBimestre,
+			int notaQuartoBimestre) {
+		return (notaPrimeiroBimestre + notaSegundoBimestre
+				+ notaTerceiroBimestre + notaQuartoBimestre) / 4.0f;
+	}
+
+	public String verificaStuacaoAluno(float media) {
+		if (media < 70.0f && media >= 50.0f) {
+			return "Em recuperação";
+		}
+
+		if (media >= 70) {
+			return "Aprovado";
+		}
+		return "Reprovado";
+	}
+
+}
